433ef6b3cb82391e819609afee88dbd5b3cb4ebd
Prashant-Baral <baralprashant09@gmail.com>
2025-07-19 14:35:01 +0530

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Student(User):
+    GENDER_CHOICES = (
+        ("Male","Male"),
+        ("Female","Female"),
+        ("Other","Other")
+    )
+    name = models.CharField(max_length=13)
+    gender = models.CharField(max_length = 10,choices= GENDER_CHOICES)
+    profile = models.ImageField(upload_to = 'user_profile')
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Student
+from django.contrib import messages
 
-# Create your views here.
 def register(request):
+    
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        new_user = User.objects.create(
+            username = username,
+            first_name = first_name,
+            last_name = last_name ,
+            email = email,
+
+        )
+
+        new_user.set_password(password) #Setting the password for the new user in the encrypted format
+
+        new_user.save()
+
+        return redirect("home")
+
     return render(request,"accounts/register.html")
 
+
+# Login
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        if Student.objects.filter(username=username).exists():
+            user = auth.authenticate(request,username=username,password=password)
+
+
+            if user is not None:
+                auth.login(request,user) # this is the function to login
+                return redirect("home")
+            messages.error(request,"Invalid Username or Password")
+
     return render(request,"accounts/login.html")
